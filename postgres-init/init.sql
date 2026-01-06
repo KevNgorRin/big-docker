e5b8f50eaf14cdadff5c898e838e22d97c0b6dbb
Kevin <kevydev@outlook.com>
2026-01-06 08:25:26 -0500

    feat: :sparkles: add Logflare service configuration and database setup

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -2,3 +2,8 @@
 CREATE USER "supertokens-user" WITH PASSWORD '12345678';
 CREATE DATABASE "supertokens-db" OWNER "supertokens-user";
 GRANT ALL PRIVILEGES ON DATABASE "supertokens-db" TO "supertokens-user";
+
+-- Create user and database for Logflare (Supabase Analytics)
+CREATE USER "logflare-user" WITH PASSWORD '12345678';
+CREATE DATABASE "logflare-db" OWNER "logflare-user";
+GRANT ALL PRIVILEGES ON DATABASE "logflare-db" TO "logflare-user";
